d5c17826e316b10147a4b7e06edc597d8111dba0
freezebreze <1156341958@qq.com>
2020-10-28 01:35:55 +0800

    three commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     publicPath: './',
-    outputDir: 'dist',//在vue-cli.3.3版本后 baseUrl被废除了，因此这边要写成 publicPath。
+    outputDir: 'docs',//在vue-cli.3.3版本后 baseUrl被废除了，因此这边要写成 publicPath。
     lintOnSave: true,
     runtimeCompiler: true, 
     chainWebpack: () => {},
